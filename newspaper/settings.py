a0e4c85a44cccc39256b6acad6778cd55acce2c8
Den <jlomuk@bk.ru>
2021-03-24 15:50:28 +0300

    change settings heroku

--- newspaper/settings.py
+++ newspaper/settings.py
@@ -1,5 +1,4 @@
 from pathlib import Path
-import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -14,7 +13,7 @@ SECRET_KEY = 'm!ebcgx5*rn3757rlg0$ajr7#n+60t@vm9ni9yl567&er17(1@'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -127,5 +126,3 @@ LOGOUT_REDIRECT_URL = 'home'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
-django_heroku.settings(locals())
