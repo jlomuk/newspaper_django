2646efe20ef49f742857be894801f8f8a21aadc6
Den <jlomuk@bk.ru>
2021-03-24 11:45:54 +0300

    add urls.py in app pages and create test

--- newspaper/urls.py
+++ newspaper/urls.py
@@ -4,8 +4,9 @@ from django.views.generic.base import TemplateView
 
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('', include('pages.urls')),
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
     path('users/', include('django.contrib.auth.urls')),
+
 ]
