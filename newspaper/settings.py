6b66ebbdcef92ecda77a41d1ff5aeedeb5239480
Den <jlomuk@bk.ru>
2021-03-24 15:59:06 +0300

    modify

--- newspaper/settings.py
+++ newspaper/settings.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,6 +79,7 @@ DATABASES = {
         'PORT': '',
     }
 }
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 
 # Password validation
